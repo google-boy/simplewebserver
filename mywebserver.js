a36ae6f36509ab1e6e74e7a7e02e3ade8d13008e
google-boy <otienoh13@gmail.com>
2022-01-21 23:14:25 +0300

    Implement HTTPS security.

--- mywebserver.js
+++ mywebserver.js
@@ -1,23 +1,35 @@
 const http = require('http')
+const https = require('https')
 const fs = require('fs').promises
+const fsa = require('fs')
 
 const host = 'localhost'
-const port = 8000
-/*
-let logFile = fs.createWriteStream("log.txt", {flags: "a"})
+const port = 8443
+
+const  options = {
+    key: fsa.readFileSync('key.pem'),
+    cert: fsa.readFileSync('cert.pem')
+}
+
+let logFile = fsa.createWriteStream("log.txt", {flags: "a"})
 let date
 
 function log(url) {
     date = new Date().toTimeString()
     logFile.write(`${date} -------> ${url}\n`)
 }
-*/    
+
 
 const  requestListener = function (req, res) {
+    /* if ((req.headers['X-Forwarded-Proto'] !== 'https')) {
+        res.writeHead(301, {"Location": `https://${req.headers['host']}${req.url}`})
+        res.headers['X-Forwarded-Proto'] = 'https'
+    }
+    */
     res.setHeader("Content-type", "text/html")
     switch (req.url) {
         case "/":
-            //log(req.url)
+            log(req.url)
             fs.readFile(__dirname + "/index.html")
                 .then(contents => {
                     res.writeHead(200)
@@ -30,7 +42,7 @@ const  requestListener = function (req, res) {
                 })
             break
         case "/about":
-            //log(req.url)
+            log(req.url)
             fs.readFile(__dirname + "/about.html")
                 .then(contents => {
                     res.writeHead(200)
@@ -43,7 +55,7 @@ const  requestListener = function (req, res) {
                 })
             break
         case "/docs":
-            //log(req.url)
+            log(req.url)
             fs.readFile(__dirname + "/docs.html")
                 .then(contents => {
                     res.writeHead(200)
@@ -56,13 +68,17 @@ const  requestListener = function (req, res) {
                 })
             break
         default:
-            //log(req.url)
+            if (!req.url === '/favicon.ico')
+            log(req.url)
             res.writeHead(404)
             res.end("Requested resource could not be found")
     }
 }
 
 const server = http.createServer(requestListener)
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`)
+server.listen(8000)
+
+const secure_server = https.createServer(options, requestListener)
+secure_server.listen(8443, host, () => {
+    console.log(`Server is running on https://${host}:${port}`)
 })
